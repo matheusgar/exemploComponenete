a9ecc4f70222ba53a7238d42e150afc343347d7d
matheusgar <matheuspereirapdsz@gmail.com>
2022-09-24 01:05:45 +0000

    alteração do projeto para calcular no click do botão

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput,TouchableOpacity } from 'react-native';
+import Dados from './components/Dados';
 
 export default function App() {
+const[nome, setNome] = useState("");
+	const[idade, setIdade] = useState("");
+	const [nomeParam, setNomeParam] = useState("");
+	const [idadeParam, setIdadeParam] = useState("");
+	
+	function gravar(){
+		setNomeParam(nome);
+		setIdadeParam(idade);
+	}
   return (
+	  
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.bloco}>
+		  <Text xtyle ={styles.label}>Nome: </Text>
+		  <TextInput			  
+			  style ={styles.input}	
+			  value={nome}
+			  onChangeText={(value) => setNome(value)}
+			  
+			  />
+		  </View>
+		  
+		  <View style = {styles.bloco}>
+		  <Text style = {styles.label}>Ano nasc: </Text>
+		  <TextInput
+			  style={styles.input}
+			  value={idade}
+			  onChangeText={(value) => setIdade(value)}
+			  
+			  />
+			  
+		  </View>
+		  
+		  <View style={styles.bloco}>	  
+		  <TouchableOpacity style = {styles.botao} onPress={gravar}>
+			  <Text style = {styles.textoBotao}>Mostrar</Text>
+			  </TouchableOpacity>
+		  </View>
+		  
+		  <View style={styles.bloco}>
+		  <Dados nome = {nomeParam} idade ={idadeParam}  />
+		  </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+	
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
+    
   },
+	
+	 bloco:{
+	  width:'80%',
+	  marginTop:30,
+	  marginLeft:'10%'
+  },
+	
+	label:{
+		
+		fontSize: 20
+		
+		
+	}, 
+	input:{
+		
+		borderWidth: 1,
+		fontSize: 20
+		
+	},
+	
+	botao:{
+	  
+	  backgroundColor:'#000',
+  },
+	
+	textoBotao:{
+	color: '#fff',
+	fontSize:20,
+	textAlign:'center'
+	
+}
 });
---
+++ components/Dados.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput,TouchableOpacity } from 'react-native';
+
+export default function App(props) {
+  return (
+	  
+	  <View>
+	  <Text style={styles.texto}>
+		  
+		  Nome:{props.nome}	
+	  </Text>
+		  
+		  <Text style = {styles.texto}>
+			  
+			  Idade:
+			  {props.idade !=""? 2022 - props.idade: ""}
+			  
+		  </Text>
+		  
+	  </View>
+	  
+	  
+	  
+	  
+  );
+}
+
+const styles = StyleSheet.create({
+	
+  container: {
+    flex: 1
+    
+  },
+	
+	 texto:{
+	  fontSize:30
+  }
+	
+});
